cdcccb0769f1266cd235d03a2c9a8623d1845a2d
Tuan Anh Le <anhlt1983@gmail.com>
2024-09-21 16:42:38 +0700

    refactor: Update BlockchainComponent layout and styles

--- src/components/DisplayBlockchainComponent/DisplayBlockchainComponent.tsx
+++ src/components/DisplayBlockchainComponent/DisplayBlockchainComponent.tsx
@@ -20,8 +20,8 @@ const DisplayBlockchainComponent: React.FC<DisplayBlockchainProps> = ({
     <div className="blockchain-view">
       <h1 className="blockchain-header">Simple Blockchain</h1>
       <p className="block-count">Number of Blocks: {blockCount}</p>
-      <button className="generate-button" onClick={generateBlock}>Generate New Block</button>
       <p className="elapsed-time">Elapsed Time: {elapsedTime} seconds</p>
+      <button className="generate-button" onClick={generateBlock}>Generate New Block</button>      
       <div id="blockchain">
         {blockchain.chain.slice().reverse().map((block, index) => (
           <BlockComponent key={index} block={block} index={blockchain.chain.length - 1 - index} isValid={blockchain.isChainValid()} />
