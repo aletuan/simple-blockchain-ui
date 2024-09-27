3f81f603f261aa4c5028946ee09a1810f932ac09
Tuan Anh Le <anhlt1983@gmail.com>
2024-09-27 20:30:07 +0700

    feat: create new address class

---
+++ src/blockchain/Address.ts
@@ -0,0 +1,30 @@
+export class Address {
+    address: string;
+    username: string;
+    amount: number = 1000;
+
+    constructor(address: string, username: string) {
+        this.username = username;
+        this.address = this.generateAddress();
+    }
+
+    private generateAddress(): string {
+        return '0x' + Math.random().toString(36).substr(2, 40);
+    }
+    
+    getShortAddress(): string {
+        return `${this.address.slice(0, 6)}...${this.address.slice(-4)}`;
+    }    
+    
+    changeAmount(value: number): void {
+        if (value > 0) {
+            this.amount += value;
+        } else if (value < 0) {
+            if (this.amount + value >= 0) {
+                this.amount += value;
+            } else {
+                throw new Error("Insufficient amount");
+            }
+        }
+    }    
+}
